558632fcf338eb16c4c6159603924efbe7ac7573
Fabrice <fabrice@linuxbe.com>
2017-09-27 10:44:25 +0200

    build but untested ... spl still to rebrand

---
+++ hotraco_spl_build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+export CC=/opt/gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux/bin/arm-linux-gnueabihf-
+
+export PRODUCT_ID_NUMBER=17065  #<product_id_number>
+export PRODUCT_VERSION_NUMBER=0      #<product_version_number>
+export PRODUCT_REVISION_NUMBER=0      #<product_revision_number>
+export PRODUCT_TEST_NUMBER=1      #<product_test_number>
+
+echo ": psiversion: $PRODUCT_ID_NUMBER.$PRODUCT_VERSION_NUMBER.$PRODUCT_REVISION_NUMBER.$PRODUCT_TEST_NUMBER" > localversion
+
+make ARCH=arm CROSS_COMPILE=${CC} distclean
+make ARCH=arm CROSS_COMPILE=${CC} hotraco_hrfocus_spl_config
+make ARCH=arm CROSS_COMPILE=${CC}
+make ARCH=arm CROSS_COMPILE=${CC} env
+
+./tools/mkimage -n board/psicontrol/hotraco_hrfocus_spl/hotraco_hrfocus.cfg.cfgtmp -T imximage -e 0x00908000 -d spl/u-boot-spl.bin spl.img
+
+# bitmap is 2.5 MB, at offset 2 MB. So I will allocate 5 MB to be sure.
+dd if=/dev/zero count=5 bs=1M | tr '\000' '\377' > hotraco_spl_firmware.bin
+dd if=spl.img of=hotraco_spl_firmware.bin bs=1K seek=1 conv=notrunc && dd if=u-boot.img of=hotraco_spl_firmware.bin bs=1K seek=64 conv=notrunc && dd if=no_sd_1024.bmp of=hotraco_spl_firmware.bin bs=1K conv=notrunc seek=2048
+
+#mv u-boot.imx u-boot_hotraco.imx
